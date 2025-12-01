86f301bbee053a6e41ab67b58a416905590b7be3
shahmeert <shahmeert511@gmail.com>
2025-12-01 11:33:32 -0500

    Initialize map

---
+++ lineMapResult.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class lineMapResult {
+    private  Map <Integer, Integer> lineMap;
+    private List <String> oldLines;
+    private List <String> newLines;
+    
+    //create intial map
+    public lineMapResult(Map<Integer, Integer> lineMap, List <String> oldLines, List <String> newLines){
+        this.lineMap = lineMap;
+        this.oldLines = oldLines;
+        this.newLines = newLines;
+    }
+
+
+
+
+
+
+
+
+
+
+}
