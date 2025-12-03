0a7f506e0178086a0e43f163e1e5d274461dd2b0
Kyle MacLeod <82135541+KyleMac24@users.noreply.github.com>
2025-12-03 14:32:26 -0500

    Added LinePair class to mapMatch to temporarily store matches

--- mapMatch.java
+++ mapMatch.java
@@ -3,9 +3,27 @@ import java.util.*;
 public class mapMatch {
 
     public lineMapResult map(List<String> oldLines, List<String> newLines){
+        
+        
         Map<Integer, Integer> mapping = new HashMap<>();
-
+            
         return new lineMapResult(mapping, oldLines, newLines);
+    }
+
+    //LinePair Class
+    private static class LinePair {
+        
+        int oldIndex;
+        int newIndex;
+
+        //Constructor
+        public LinePair(int oldIndex, int newIndex) {
+
+            this.oldIndex = oldIndex;
+            this.newIndex = newIndex;
+
+        } 
+
     }
 
 }
