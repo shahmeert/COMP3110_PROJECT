73e3e4926696d83497be05533c46cdff4e2c74ec
Kyle MacLeod <82135541+KyleMac24@users.noreply.github.com>
2025-12-03 13:47:46 -0500

    Modified DSLoader to use normalizeFile.java to create normalized lists of file lines

--- DSLoader.java
+++ DSLoader.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class DSLoader {
@@ -9,18 +9,20 @@ public class DSLoader {
 
         File folder = new File(path);
         
-
+        //Folder path entered incorrectly
         if (!folder.exists() || !folder.isDirectory()){
             System.out.println("Dataset not found");
             return testList;
         }
 
+        //Create array of files
         File[] files = folder.listFiles();
 
         for (int i = 0; i < files.length - 1; i++){
             File oldFile = files[i];
             File newFile = files[i + 1];
 
+            //Remove excess from title to determine base
             String oldBase = oldFile.getName().replaceAll("\\d+\\.txt$", "");
             String newBase = newFile.getName().replaceAll("\\d+\\.txt$", "");
 
@@ -28,13 +30,22 @@ public class DSLoader {
                 continue;
             }
 
-            List<String> oldLines = Files.readAllLines(oldFile.toPath());
-            List<String> newLines = Files.readAllLines(newFile.toPath()); 
+            //Send Files to normalizeFile.java for normalization
+            Path oldPath = oldFile.toPath();
+            Path newPath = newFile.toPath();
 
+            List<String> oldLines = normalizeFile.readNormalFile(oldPath);
+            List<String> newLines = normalizeFile.readNormalFile(newPath);
+
+            //Name the test case
             String testName = oldBase + (i + 1) + "→" + oldBase + (i + 2);
 
+            //Add test case to list of test cases
             testList.add(new TestCase(testName, oldLines, newLines));
         }
+
         return testList;
+    
     }
+
 }
