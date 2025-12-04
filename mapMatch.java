351f53db192690f827f54eabfacd807c1e204c4c
Kyle MacLeod <82135541+KyleMac24@users.noreply.github.com>
2025-12-04 12:58:12 -0500

    Added Levenshtein algorithm with helper methods

--- mapMatch.java
+++ mapMatch.java
@@ -57,6 +57,7 @@ public class mapMatch {
         return exactMatches;
     }
 
+    //Helper class to store match info
     private static class CandidateMatch{
         int oldIndex;
         int newIndex;
@@ -70,10 +71,62 @@ public class mapMatch {
 
     }
 
+    //Helper Class to calculate content and context similarity
+    private static class Similairty{
+
+
+        //Levenshtein Algorithm
+        private static int levenshteinDistance(String s1, String s2){
+
+            int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+            for(int i = 0; i <= s1.length(); i++){
+
+                for(int j = 0; j <= s2.length(); j++){
+
+                    if(i == 0){
+
+                        dp[i][j] = j;
+
+                    }else if(j == 0) {
+                        
+                        dp[i][j] = i;
+
+                    }else{
+
+                        dp[i][j] = min(dp[i - 1][j - 1] + costOfSubstitution(s1.charAt(i - 1), s2.charAt(j - 1)), dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+
+                    }
+
+                }
+
+
+            }
+
+            return dp[s1.length()][s2.length()];
+
+        }
+
+       //Helper methods for levenshtein
+       public static int costOfSubstitution(char a, char b){
+
+            return a == b ? 0 : 1;
+
+       }
+
+       public static int min(int... numbers){
+
+            return Arrays.stream(numbers).min().orElse(Integer.MAX_VALUE);
+
+       }
+
+
+    }
+
     private List<CandidateMatch> findCandidates(List<String> oldLines, List<String> newLines, Map<Integer, Integer> exactMatchesMap){
 
 
-        
+
     }
 
 }
