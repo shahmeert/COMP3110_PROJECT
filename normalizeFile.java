a6e40ea3bbf2c526e200d3defd78058fe244a54e
shahmeert <shahmeert511@gmail.com>
2025-12-06 10:52:43 -0500

    Added a fix to deal with file comments + fixed formatting issues

--- normalizeFile.java
+++ normalizeFile.java
@@ -15,26 +15,89 @@ import java.util.List;
 
 public class normalizeFile {
     
-        public static List<String> readNormalFile(Path file) throws IOException{
-            List<String> lines = new ArrayList<>();
+    public static List<String> readNormalFile(Path file) throws IOException{
+        List<String> lines = new ArrayList<>();
 
-            try(BufferedReader br = Files.newBufferedReader(file)){
-                String line;
+        try(BufferedReader br = Files.newBufferedReader(file)){
+            String line;
 
-                while((line = br.readLine()) != null){
-                    String normalize = normalize(line);
-                    if(normalize.isEmpty() != true){
-                        lines.add(normalize);
+            while((line = br.readLine()) != null){
+                lines.add(line);
+            }
+        }
+        List<String> noComments = removeComments(lines); 
+        List<String> result = new ArrayList<>();
+        for (String line : noComments) {
+            String normalized = normalize(line);
+            if (!normalized.isEmpty()) {
+                result.add(normalized);
+            }
+        }
+        return lines;
+    }
+
+    private static List<String> removeComments(List<String> lines){
+        List<String> result = new ArrayList<>();
+        boolean inBlock = false;
+
+        for(String line : lines){
+            StringBuilder sb = new StringBuilder();
+            boolean inString = false;
+            char stringChar = 0;
+
+            int i = 0;
+            int n = line.length();
+
+            while(i < n){
+                char c = line.charAt(i);
+
+                if(!inBlock){ //handles string literals
+
+                    if(c == '"' || c == '\''){
+                        if(!inString){
+                            inString = true;
+                            stringChar = c;
+                        } else if(c == stringChar){
+                            if(i > 0 && line.charAt(i - 1) != '\\'){
+                                inString = false;
+                            }
+                        }
+                    }
+                    sb.append(c);
+                    i++;
+                }
+                else if(!inString){ //looks for comments outside of string
+                    if(i + 1 < n && c == '/' && line.charAt(i + 1) == '*' ){ //block comments
+                        inBlock = true;
+                        i += 2;
+                    }
+                    else if( i + 1 < n && c =='/' && line.charAt(i + 1) == '/'){ //single line comment
+                        break;
+                    }
+                    else{ //inside string normal char
+                        sb.append(c);
+                        i++;
+                    }
+
+                }
+                else{
+                    if(i + 1 < n && c == '*' && line.charAt(i +1) == '/'){
+                        inBlock = false;
+                        i += 2;
+                    } else{
+                        i++;
                     }
                 }
-            }
-            return lines;
         }
+            result.add(sb.toString());
+        }
+        return result;
+    }
 
-        private static String normalize(String line){
-            line = line.trim();
-            line = line.replaceAll("\\s+", " ");
-            return line;
-        }
+    private static String normalize(String line){
+        line = line.trim();
+        line = line.replaceAll("\\s+", " ");
+        return line;
+    }
 
 }
