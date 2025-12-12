257064f06ad494d74da972fcbce94803bd36f1a2
markobilek914-hue <marko.bilek914@gmail.com>
2025-12-12 16:33:38 -0500

    Added accuracy calc
    Computes the accuracy of the test cases

--- lineMapResult.java
+++ lineMapResult.java
@@ -2,14 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class lineMapResult {
-    private final Map <Integer, Integer> lineMap;
-    
+    private Map <Integer, Integer> lineMap = new LinkedHashMap<>();
+    private double accuracy = -1;
     //Constructor
     public lineMapResult(Map<Integer, Integer> lineMap){
-       
         this.lineMap = lineMap;
-
     }
+
+    public void addMapping(int oldLine, int newLine) {
+        lineMap.put(oldLine, newLine);
+    }
+
+    public void setAccuracy(double acc) {
+        this.accuracy = acc;
+    }
+
+    public double computeAccuracy(Map<Integer, Integer> expected){
+        int correct = 0;
+
+        // compare tests and count correct test
+        for(int orgin : expected.keySet()){
+            int expectedVal = expected.get(orgin);
+            int actualVal = lineMap.getOrDefault(orgin, -2);
+
+            if(expectedVal == actualVal){ 
+                correct++;
+            }
+        }
+        // return percent of correct tests
+        return (100.00 * correct) / expected.size();
+    }
+
 
     public void printToFile(String filename){
 
