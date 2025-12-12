bde85714e5e97db28df09922bbb25b60f5ab3b0f
Kyle MacLeod <82135541+KyleMac24@users.noreply.github.com>
2025-12-12 15:28:24 -0500

    Changed TestCase to store xml info and added xml files

--- TestCase.java
+++ TestCase.java
@@ -1,13 +1,17 @@
-import java.util.List;
+import java.util.*;
 
 public class TestCase {
     public String name;
     public List<String> oldLines;
     public List<String> newLines;
+    public Map<Integer, Integer> expectedMapping;
+    public boolean hasXml;
 
-    public TestCase(String name, List<String> oldLines, List<String> newLines ){
+    public TestCase(String name, List<String> oldLines, List<String> newLines, Map<Integer, Integer> expectedMapping, boolean hasXml){
         this.name = name;
         this.oldLines = oldLines;
         this.newLines = newLines;
+        this.expectedMapping = expectedMapping;
+        this.hasXml = hasXml;
     }
 }
